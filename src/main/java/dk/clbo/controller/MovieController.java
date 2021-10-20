e2eead8a000ead74a93c8220cb6236ec74157601
Casper Steengaard <casper.aakviststeengaard@gmail.com>
2021-10-20 11:06:11 +0200

    it works

--- src/main/java/dk/clbo/controller/MovieController.java
+++ src/main/java/dk/clbo/controller/MovieController.java
@@ -8,10 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MovieController {
 
-    MovieService service = new MovieService();
+    MovieService service;
 
     @GetMapping("/")
     public String index(Model model){
+        service = new MovieService();
         model.addAttribute("movies", service.getAllMovies());
         return "index";
     }
